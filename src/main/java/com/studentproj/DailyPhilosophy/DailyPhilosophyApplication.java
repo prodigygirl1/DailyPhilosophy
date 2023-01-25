9051c603ca60a913a8845774aad25c9ebac2189c
prodigygirl <Prodigygirl1.0@yandex.ru>
2023-01-25 13:11:32 +0400

    Initial Commit

---
+++ src/main/java/com/studentproj/DailyPhilosophy/DailyPhilosophyApplication.java
@@ -0,0 +1,13 @@
+package com.studentproj.DailyPhilosophy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyPhilosophyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyPhilosophyApplication.class, args);
+	}
+
+}
